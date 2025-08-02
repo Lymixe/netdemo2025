2639c87603fe4fa5128635979f3ab1b3d5c4d38f
Lymixe <peraltacenepoemily@gmail.com>
2025-08-02 02:30:23 -0500

    Tarea - Formulario Web

---
+++ Models/JugadorLOL.cs
@@ -0,0 +1,13 @@
+namespace NETDEMO.Models;
+public class JugadorLOL
+
+{
+    public string? nombre { get; set; }
+
+    public int? edad { get; set; }
+
+    public string? frecuencia { get; set; }
+
+    public string? añosJ { get; set; }
+    
+}
